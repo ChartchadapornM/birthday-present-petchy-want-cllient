f3144ce11d131fd8452de1d828edc10d6dd3eb21
ChartchadapornM <chartchadaporn.m@gmail.com>
2025-01-17 15:00:13 +0700

    Fetch and create product.

---
+++ src/app/addpresent/page.js
@@ -0,0 +1,104 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import { Poppins } from "next/font/google";
+import { useRouter } from "next/navigation";
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
+
+const AddPresentPage = () => {
+  const [formProduct, setFormProduct] = useState({
+    name: "",
+    img: "",
+    desc: "",
+    price: "",
+    type: "Hot Deals",
+  });
+  const router = useRouter();
+
+  const onChangeProductData = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
+    setFormProduct((prevState) => ({ ...prevState, [name]: value }));
+  };
+
+  const productSubmitForm = async (e) => {
+    e.preventDefault();
+    await fetch("http://localhost:8002/add-product", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(formProduct),
+    });
+    router.push("/");
+  };
+
+  return (
+    <main className={poppins.className}>
+      <div className="container px-16">
+        <p className="text-5xl font-extrabold text-gray-900 underline text-center my-4">
+          birthday present petchy want
+        </p>
+        <p className="text-3xl mt-8">Add more</p>
+        <div className="mb-6">
+          <form onSubmit={productSubmitForm}>
+            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white mt-2">
+              Product name.
+            </label>
+            <input
+              onChange={onChangeProductData}
+              name="name"
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white mt-2">
+              Image for the product.
+            </label>
+            <input
+              onChange={onChangeProductData}
+              name="img"
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white mt-2">
+              Description for the product.
+            </label>
+            <input
+              onChange={onChangeProductData}
+              name="desc"
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white mt-2">
+              Price of the product.
+            </label>
+            <input
+              onChange={onChangeProductData}
+              name="price"
+              type="text"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+            <label className="block mb-2 text-lg font-medium text-gray-900 dark:text-white mt-2">
+              Select an option.
+            </label>
+
+            <select
+              onChange={onChangeProductData}
+              name="type"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            >
+              <option value="Hot Deals">Hot deals</option>
+              <option value="Clothes">Clothes</option>
+              <option value="Others">Others</option>
+            </select>
+            <button
+              type="Submit"
+              className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800 mt-6"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default AddPresentPage;
--- src/app/page.js
+++ src/app/page.js
@@ -1,101 +1,136 @@
+"use client";
 import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+const poppins = Poppins({ weight: "400", subsets: ["latin"] });
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [hotdeals, setHotdeals] = useState([]);
+  const [clothes, setClothes] = useState([]);
+  const [others, setOthers] = useState([]);
+  const router = useRouter();
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+  useEffect(() => {
+    fetchhotdeals();
+    fetchclothes();
+    fetchothers();
+  }, []);
+
+  const fetchhotdeals = async () => {
+    const respones = await fetch("http://localhost:8002/product-hotdeals");
+    const data = await respones.json();
+    setHotdeals(data);
+  };
+  const fetchclothes = async () => {
+    const respones = await fetch("http://localhost:8002/product-clothes");
+    const data = await respones.json();
+    setClothes(data);
+  };
+  const fetchothers = async () => {
+    const respones = await fetch("http://localhost:8002/product-others");
+    const data = await respones.json();
+    setOthers(data);
+  };
+  const onAddButton = () => {
+    router.push("/addpresent");
+  };
+
+  return (
+    <main className={poppins.className}>
+      <div className="container px-16">
+        <p className="text-5xl font-extrabold text-gray-900 underline text-center my-4">
+          birthday present petchy want
+        </p>
+        <div className="flex flex-col items-center">
+          <button
+            type="button"
+            onClick={onAddButton}
+            className="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800  "
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            Add new present
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+        <p className="font-bold text-2xl">HOT DEALS🔥</p>
+        <div className="mt-4 grid grid-cols-4 gap-12">
+          {hotdeals.map((value) => {
+            return (
+              <div
+                className="max-w-sm rounded overflow-hidden shadow-lg border-4 rounded-lg border-red-500 bg-stone-200"
+                key={value.id}
+              >
+                <img
+                  className="w-full"
+                  src={value.img}
+                  alt="Sunset in the mountains"
+                />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{value.name}</div>
+                  <p className="text-gray-700 text-base">{value.desc}</p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #{value.price} Baht
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <p className="font-bold text-2xl mt-4">Clothes</p>
+        <div className="mt-4 grid grid-cols-4 gap-12">
+          {clothes.map((value) => {
+            return (
+              <div
+                className="max-w-sm rounded overflow-hidden shadow-lg border-4 rounded-lg bg-stone-200"
+                key={value.id}
+              >
+                <img
+                  className="w-full"
+                  src={value.img}
+                  alt="Sunset in the mountains"
+                />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{value.name}</div>
+                  <p className="text-gray-700 text-base">{value.desc}</p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #{value.price} Baht
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <p className="font-bold text-2xl mt-4">Others</p>
+        <div className="mt-4 grid grid-cols-4 gap-12">
+          {others.map((value) => {
+            return (
+              <div
+                className="max-w-sm rounded overflow-hidden shadow-lg border-4 rounded-lg bg-stone-200"
+                key={value.id}
+              >
+                <img
+                  className="w-full"
+                  src={value.img}
+                  alt="Sunset in the mountains"
+                />
+                <div className="px-6 py-4">
+                  <div className="font-bold text-xl mb-2">{value.name}</div>
+                  <p className="text-gray-700 text-base">{value.desc}</p>
+                </div>
+                <div className="px-6 pt-4 pb-2">
+                  <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                    #{value.price} Baht
+                  </span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
   );
 }
